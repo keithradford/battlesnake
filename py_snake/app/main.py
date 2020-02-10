54a9df1dac7caa5b12df9221e80a347a17250ecd
Keith Radford <53020925+keithradford@users.noreply.github.com>
2020-02-10 13:38:02 -0800

    Customized snake, added closest food detection

--- py_snake/app/main.py
+++ py_snake/app/main.py
@@ -45,9 +45,11 @@ def start():
     """
     print(json.dumps(data))
 
-    color = "#00FF00"
+    color = "#2E86C1"
+    headType = "bendr"
+    tailType = "bwc-ice-skate"
 
-    return start_response(color)
+    return start_response(color, headType, tailType)
 
 
 @bottle.post('/move')
@@ -61,9 +63,43 @@ def move():
     print(json.dumps(data))
 
     directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
+    direction = directions[1]
+    up = directions[0]
+    down = directions[1]
+    left = directions[2]
+    right = directions[3]
 
-    return move_response(direction)
+    snake = data['you']['body'][0]
+    height = data['board']['height']
+    width = data['board']['width']
+    food = data['board']['food']
+
+    closest_food = get_closest_food(food, snake)
+    tmp_food = closest_food
+
+    # Move towards closest food, pretty up later
+    if(tmp_food[0][0] > 0):
+        while(tmp_food[0][0] > 0):
+            return move_response(left)
+    elif(tmp_food[0][0] < 0):
+        while(tmp_food[0][0] < 0):
+            return move_response(right)
+
+    if(tmp_food[0][1] > 0):
+        while(tmp_food[0][1] > 0):
+            return move_response(up)
+    elif(tmp_food[0][1] < 0):
+        while(tmp_food[0][1] < 0):
+            return move_response(down)
+
+    # if(snake['x'] == 0):
+    #     return move_response(down)
+    # if(snake['y'] == height):
+    #     return move_response(right)
+    # elif(snake['x'] == width and snake['y'] == 0):
+    #     return move_response(up)
+    # elif(snake['y'] == 0 and snake['x'] == width):
+    #     return move_response(left)
 
 
 @bottle.post('/end')
@@ -77,6 +113,33 @@ def end():
     print(json.dumps(data))
 
     return end_response()
+
+# get_closest_food
+# Parameters: list of food coordinates, snake head coordinates
+# Returns: sorted list of coordinates to closest food
+def get_closest_food(food, snake):
+    size = len(food)
+    snake_loc = (snake['x'], snake['y'])
+    food_loc = []
+    for i in range(size):
+        x, y = food[i]['x'], food[i]['y']
+        food_loc.append((x, y))
+    closest_food = [((snake_loc[0]-food_loc[i][0]), (snake_loc[1]-food_loc[i][1])) for i in range(size)]
+
+    return sorted(closest_food, key  = add_coord)
+
+# add_coord
+# Parameters: list item
+# Returns: sum of coordinates
+# If a number is negative, make it positive as this function serves to help sort coordinates in terms of relative position to snake    
+def add_coord(item):
+    tmp_x = item[0]
+    tmp_y = item[1]
+    if(item[0] < 0):
+        tmp_x *= -1
+    if(item[1] < 0):
+        tmp_y *= -1
+    return tmp_x + tmp_y
 
 
 # Expose WSGI app (so gunicorn can find it)
